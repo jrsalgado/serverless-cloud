9276161759791140556f62b486014f6f72bdfa2f
jrsalgado <jayrosalgado@gmail.com>
2023-11-03 10:38:36 -0700

    Add the pipeline to CDK project

--- cdk-cicd/lib/cdk-cicd-stack.ts
+++ cdk-cicd/lib/cdk-cicd-stack.ts
@@ -1,16 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkCicdQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CodePipeline(this, 'ApplicationPipeline', {
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('jrsalgado/serverless-cloud', 'master'),
+        commands: [ 'cd cdk-cicd', 'npm ci', 'npx cdk synth' ],
+        primaryOutputDirectory: 'cdk-cicd/cdk.out'
+      })
+    })
   }
 }
