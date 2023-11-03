253db6a738d5588c0b2060358dae8f3f0c20e7ea
jrsalgado <jayrosalgado@gmail.com>
2023-11-03 11:59:40 -0700

    add product-picker lambda

--- cdk-cicd/lib/serverless-stack.ts
+++ cdk-cicd/lib/serverless-stack.ts
@@ -1,5 +1,8 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs"
+import { join } from "path"
 
 interface ServerlessStackProps extends StackProps{
     stageName?: string;
@@ -8,5 +11,11 @@ interface ServerlessStackProps extends StackProps{
 export class ServerlessStack extends Stack {
     constructor(scope: Construct, id: string, props: ServerlessStackProps) {
         super(scope, id, props)
+
+        new NodejsFunction(this, 'product-picker', {
+            runtime: Runtime.NODEJS_18_X,
+            handler: 'handler',
+            entry: (join(__dirname, '../..', 'product-picker/src', 'index.ts'))
+        })
     }
 }
